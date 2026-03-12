a35f37e5ddde6b14eb95198d8dca765cd4ee8517
theun1c <belovdima1122@yandex.ru>
2026-03-12 06:34:05 +0300

    feat: add test player

--- src/Pika.Desktop/Views/MainWindow.axaml.cs
+++ src/Pika.Desktop/Views/MainWindow.axaml.cs
@@ -1,5 +1,7 @@
 
 using Avalonia.Controls;
+using Pika.Desktop.Controls;
+
 namespace Pika.Desktop.Views;
 
 public partial class MainWindow : Window
@@ -8,5 +10,9 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+        
+        var player = this.FindControl<VideoGlControl>("PlayerControl");
+        
+        player?.SetSource("/home/theun1c/Downloads/output.mp4");
     }
 }
