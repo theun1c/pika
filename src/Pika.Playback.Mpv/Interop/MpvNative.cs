87332e51d5edfb6b75076b49efdd9a1e8c506661
theun1c <belovdima1122@yandex.ru>
2026-03-12 03:52:05 +0300

    feat: add base mpv native

--- src/Pika.Playback.Mpv/Interop/MpvNative.cs
+++ src/Pika.Playback.Mpv/Interop/MpvNative.cs
@@ -16,4 +16,19 @@ internal static partial class MpvNative
     [LibraryImport("libmpv.so.2")]
     [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
     internal static partial void mpv_destroy(nint ctx);
+    
+    [LibraryImport("libmpv.so.2",
+        StringMarshalling = StringMarshalling.Utf8)]
+    [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+    internal static partial int mpv_set_option_string(nint ctx, string option, string value);
+    
+    [LibraryImport("libmpv.so.2",
+        StringMarshalling = StringMarshalling.Utf8)]
+    [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+    internal static partial int mpv_command_string(nint ctx, string args);
+    
+    [LibraryImport("libmpv.so.2")]
+    [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+    internal static partial nint mpv_error_string(int error);
+    
 }
